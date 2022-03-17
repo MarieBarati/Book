a67bd287d965718f7ebe5c9859d30f8f8f385e4d
Marie Barati <Barati.it91@gmail.com>
2022-03-17 13:57:18 -0700

    Category Controller is created

---
+++ Book/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
