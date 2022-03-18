fff47c0db615f5441c661ed268bacdfe3c49710a
Marie Barati <Barati.it91@gmail.com>
2022-03-17 18:44:37 -0700

    Add Bootswatch Theme

--- Book/Controllers/CategoryController.cs
+++ Book/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Book.Data;
+using Book.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Book.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly  ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
