a312c387aebc9191ba3d577629a528e0406ae54e
Marie Barati <Barati.it91@gmail.com>
2022-03-18 14:23:58 -0700

    Edit is created

--- Book/Controllers/CategoryController.cs
+++ Book/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ namespace Book.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly  ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db;
 
         public CategoryController(ApplicationDbContext db)
         {
@@ -19,16 +19,18 @@ namespace Book.Controllers
         }
         //Get
         public IActionResult Create()
-        { 
+        {
             return View();
         }
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]//To Avoid Cross-site request forgery
         public IActionResult Create(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("CustomError","Check your information");
+                ModelState.AddModelError("CustomError", "Check your information");
             }
             if (ModelState.IsValid)
             {
@@ -37,8 +39,45 @@ namespace Book.Controllers
                 return RedirectToAction("Index");
             }
             else
-            { 
-                return  View(obj);
+            {
+                return View(obj);
+            }
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFormDb = _db.Categories.Find(id);
+            if(categoryFormDb == null)
+            {
+                return NotFound();
+            } 
+
+            return View(categoryFormDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]//To Avoid Cross-site request forgery
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Check your information");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
             }
         }
     }
