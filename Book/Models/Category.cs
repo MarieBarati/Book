dff48e0cd479c46302fa4bfaf1c7ad4f5220071e
Marie Barati <Barati.it91@gmail.com>
2022-03-18 13:40:02 -0700

    Display Name and Range Validation

--- Book/Models/Category.cs
+++ Book/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Book.Models
 {
@@ -8,6 +9,8 @@ namespace Book.Models
         public int  Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("DisplayOrder")]
+        [Range(1,100,ErrorMessage ="Display orger most be between 1 and 100!")]
         public int DisplayOrder { get; set; }
         public DateTime dateTime { get; set; }  =   DateTime.Now;
 
